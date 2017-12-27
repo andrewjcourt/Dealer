950840a69c7f43a4addefa23697e46f6f5b60d9f
Andrew <33779929+andrewjcourt@users.noreply.github.com>
2017-12-27 20:30:59 +1100

    Add files via upload

---
+++ dealer.py
@@ -0,0 +1,57 @@
+# shuffler
+# BUILD a standard 52-card deck (text representation) SHUFFLE it, DISPLAY it, and DEAL hands
+
+def build_deck():
+    face = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    suit = [u'\u2665'.encode('utf_8'), u'\u2666'.encode('utf_8'),
+            u'\u2663'.encode('utf_8'), u'\u2660'.encode('utf_8')]
+    deck = []
+    deck = [str(f + s) for s in suit for f in face]
+    return deck
+
+def shuffle():
+    stack = [None] * 52
+    deck = build_deck()
+    import random
+    for i in range(0, 52):
+        n = random.randint(0, len(deck)-1)
+        stack[i] = deck[n]
+        deck.pop(n)
+    return stack
+
+def display(stack):
+    for i in range(0, len(stack)):
+        print stack[i],
+
+def deal(x, stack):
+    hand = stack[0:x]
+    stack = stack[x:]
+    return hand, stack
+
+# deck = shuffle()
+# display(deck) or display(shuffle())
+# hand = deal(x, deck) ... returns hand[0] (hand of x cards) and hand[1] (the remaining stack)
+
+# example: shuffle deck, display shuffled deck, and deal & display four hands of seven cards:
+
+deck = shuffle()
+display(deck)
+print; print
+for i in range(0, 4):
+    hand = deal(7, deck)
+    display(hand[0])
+    print
+    deck = hand[1]
+
+
+
+
+
+
+   
+
+
+
+
+
+
